a3dde1871f7e1fd37bc4b5b14bd71cbcbe1041eb
khaled <khaledb.yahya@gmail.com>
2022-12-16 21:49:48 +0100

    fixes

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import time
 from gtts import gTTS
 import os
 import threading
+import sys
+
 
 window = tk.Tk()
 window.resizable(width=False, height=False)
@@ -41,25 +43,49 @@ percent_label = ttk.Label(master=window, textvariable=percent_var)
 progress_bar.place(x=200, y=0, width=200, height=30)
 percent_label.place(x=400, y=0, width=30, height=30)
 
-
+cancel_button_clicked = False
 def process_pdf(filelocation):
-    # Open the selected file and extract the text using PyPDF2
+    global cancel_button_clicked
+
     with open(filelocation, "rb") as f:
         pdf = PyPDF2.PdfFileReader(f)
         myText = ""
         progress_bar.config(maximum=pdf.numPages)
         for pageNum in tqdm((range(pdf.numPages))):
-            time.sleep(0.3)
+            #time.sleep(0.3)
             pageObj = pdf.getPage(pageNum)
             myText += pageObj.extractText()
+            progress_lock.acquire()
             progress_bar.step()
+            progress_lock.release()
+
+            if cancel_button_clicked:
+                window.destroy()
+                break
 
     fileOutput = gTTS(text=myText, lang="en")
     print("Generating Speech...")
-    value = fileOutput.save("audio.mp3").time()
+    message_label = tk.Label(master=window, text="Generating Speech...")
+    message_label.place(x=240, y=50)
+    window.update()
+
+    def save_audio(fileOutput, t):
+        fileOutput.save("audio.mp3", t=t)
+
+    with tqdm(unit="KB", unit_scale=True, miniters=1, desc="Saving audio file") as t:
+        save_audio(fileOutput, t)
+    message_label.destroy()
 
     os.system("start audio.mp3")
     print("Successfully Generated!!")
+
+def cancel_button_click():
+    global cancel_button_clicked
+    cancel_button_clicked = True
+
+
+cancel_button = tk.Button(master=window, text="Cancel", command=cancel_button_click)
+cancel_button.grid(row=2, column=0, sticky="nsew")
 
 def btn():
     print("Button clicked!")
